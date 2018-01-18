a471d3dc2d10348fb08cc769dc670df3f8834cdf
alex <alex@debian>
2018-01-18 10:45:22 -0500

    -Assload of documentation -board design complete

--- Biopoly/src/Biopoly.java
+++ Biopoly/src/Biopoly.java
@@ -1,43 +1,106 @@
-
 import processing.core.*;
 import g4p_controls.*;
+import java.io.File;
 import javax.swing.*;
 
-//change name of class:
+/**
+ *
+ * @author k24alex
+ */
 public class Biopoly extends PApplet {
 
     static String[] program = {"Biopoly"};
 
     public void setup() {
         size(640, 480, JAVA2D);
-        int players; //players
-        int rounds; //number of rounds
+        /**
+         * Number of players playing the game.
+         */
+        int players;
         
-        //asks how many rounds (shortens game play)
+        /**
+         * Number of rounds the game will reach.
+         */
+        int rounds;
+        
+        /**
+         * Stores x and y locations of board location, comes from csv file.
+         */
+        int positions[][] = new int[40][2];
+        
+        /**
+         * Stores knowledge questions.
+         */
+        String knowq[] = new String[80];
+        
+        /**
+         * Stores knowledge question answers.
+         */
+        String ansq[] = new String[80];
+        
+        /**
+         * stores multiple choice questions.
+         */
+        String mcq[] = new String[20];
+        
+        /**
+         * stores multiple choice question answers.
+         */
+        String ansmcq[] = new String[20];
+        
+        /**
+         * file that stores the positions of tiles.
+         */
+        File pos = new File("positions.csv");
+        
+        /**
+         * file that stores knowldege questions and answers.
+         */
+        File know = new File("knowledge.txt");
+        
+        /**
+         * file that stores multiple choice questions and answers.
+         */
+        File multiple = new File("multiplechoice.txt");
+        
+
+        /**
+         * User inputs the amount of rounds they wish the game will last, minimum of 3.
+         */
         while (true) {
             rounds = Integer.parseInt(JOptionPane.showInputDialog("How many rounds? (min 3)"));
             if (rounds >= 3) {
                 break;
             }
         }
-        
-        //asks for amount of players min 1, max 6
+
+        /**
+         * User inputs the amount of players in the game.
+         */
         while (true) {
             players = Integer.parseInt(JOptionPane.showInputDialog("How many players? (max 6)"));
             if (players >= 1 && players <= 6) {
                 break;
             }
         }
+
+        /**
+         * Stores users' name, will be used to identify users later.
+         */
+        String names[] = new String[players];
         
-        //2 parallel arrays
-        String names[] = new String[players]; //names
+        /**
+         * Stores the round each player is at, and their amount of money.
+         */
         int data[][] = new int[players][2]; //current round, money
-        
-        //loop asks for players' names
+
+        /**
+         * User Inputs their name.
+         */
         for (int i = 0; i < players; i++) {
-            names[i] = JOptionPane.showInputDialog("What is player "+(i+1)+"'s name?"); 
+            names[i] = JOptionPane.showInputDialog("What is player " + (i + 1) + "'s name?");
         }
-        
+
     }
 
     public void handleButtonEvents(GButton button, GEvent event) {
@@ -55,4 +118,14 @@ public class Biopoly extends PApplet {
         PApplet.main(program);
     }
 
+    /**
+     * Checks if the the player has finished the game
+     *
+     * @param t this is the players current round 
+     * @param m this is the amount of rounds the game will go to
+     * @return true if the player finished the last round, otherwise false
+     */
+    public static boolean checkWin(int t, int m) {
+        return t > m;
+    }
 }
